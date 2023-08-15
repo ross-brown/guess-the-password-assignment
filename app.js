5bda02eb88dc5e36797b211974a42efd75b1eb92
ross-brown <rbrown_98@yahoo.com>
2023-08-15 10:39:29 -0500

    refactor app.js to es2015

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var wordCount = 10;
-  var guessCount = 4;
-  var password = '';
+document.addEventListener('DOMContentLoaded', function () {
+  const wordCount = 10;
+  let guessCount = 4;
+  let password = '';
 
-  var start = document.getElementById('start');
-  start.addEventListener('click', function() {
+  const start = document.getElementById('start');
+  start.addEventListener('click', function () {
     toggleClasses(document.getElementById('start-screen'), 'hide', 'show');
     toggleClasses(document.getElementById('game-screen'), 'hide', 'show');
     startGame();
   });
 
-  function toggleClasses(element) {
-    for (var i = 1; i < arguments.length; i++) {
-      element.classList.toggle(arguments[i]);
+  function toggleClasses(element, ...classes) {
+    for (let i = 0; i < classes.length; i++) {
+      element.classList.toggle(classes[i]);
     }
   }
 
   function startGame() {
     // get random words and append them to the DOM
-    var wordList = document.getElementById('word-list');
+    const wordList = document.getElementById('word-list');
     // 'words' variable is from words.js
-    var randomWords = getRandomValues(words, wordCount); // eslint-disable-line no-undef
-    randomWords.forEach(function(word) {
-      var li = document.createElement('li');
+    const randomWords = getRandomValues(words, wordCount); // eslint-disable-line no-undef
+    randomWords.forEach((word) => {
+      const li = document.createElement('li');
       li.innerText = word;
       wordList.appendChild(li);
     });
@@ -40,32 +40,32 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   function shuffle(array) {
-    var arrayCopy = array.slice();
-    for (var idx1 = arrayCopy.length - 1; idx1 > 0; idx1--) {
+    const arrayCopy = array.slice();
+
+    for (let idx1 = arrayCopy.length - 1; idx1 > 0; idx1--) {
       // generate a random index between 0 and idx1 (inclusive)
-      var idx2 = Math.floor(Math.random() * (idx1 + 1));
+      const idx2 = Math.floor(Math.random() * (idx1 + 1));
 
       // swap elements at idx1 and idx2
-      var temp = arrayCopy[idx1];
-      arrayCopy[idx1] = arrayCopy[idx2];
-      arrayCopy[idx2] = temp;
+      [arrayCopy[idx1], arrayCopy[idx2]] = [arrayCopy[idx2], arrayCopy[idx1]]
     }
+
     return arrayCopy;
   }
 
   function setGuessCount(newCount) {
     guessCount = newCount;
     document.getElementById('guesses-remaining').innerText =
-      'Guesses remaining: ' + guessCount + '.';
+      `Guesses remaining: ${guessCount}.`;
   }
 
   function updateGame(e) {
     if (e.target.tagName === 'LI' && !e.target.classList.contains('disabled')) {
       // grab guessed word, check it against password, update view
-      var guess = e.target.innerText;
-      var similarityScore = compareWords(guess, password);
+      const guess = e.target.innerText;
+      const similarityScore = compareWords(guess, password);
       e.target.classList.add('disabled');
-      e.target.innerText = guess + ' --> Matching Letters: ' + similarityScore;
+      e.target.innerText = `${guess} --> Matching Letters: ${similarityScore}`;
       setGuessCount(guessCount - 1);
 
       // check whether the game is over
@@ -83,8 +83,8 @@ document.addEventListener('DOMContentLoaded', function() {
     if (word1.length !== word2.length) {
       throw 'Words must have the same length';
     }
-    var count = 0;
-    for (var i = 0; i < word1.length; i++) {
+    let count = 0;
+    for (let i = 0; i < word1.length; i++) {
       if (word1[i] === word2[i]) count++;
     }
     return count;
